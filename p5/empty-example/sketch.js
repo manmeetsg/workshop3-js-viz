4a27f2a3fa2a387e791022113969db12ea6772e7
Manmeet <manmeetsgujral@gmail.com>
2016-07-14 15:52:05 -0400

    Changed paths

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-
+  canvas = createCanvas(window.innerWidth, window.innerHeight);
 }
 
 function draw() {
-
+  stroke(255,127,80);
+  fill(40,70,100);
+  if (mouseIsPressed) {
+    fill(153,51,255);
+  }
+  if (keyIsPressed) {
+    fill(0,204,102);
+  }
+  ellipse(mouseX,mouseY,80,80);
 }
